a54d1829bea9c5e335253c789948786345999b43
adgor <adgor@pld-linux.org>
2006-10-01 22:56:39 +0000

    - Backward compatibility
    Changed files:
        fileutils.sh -> 1.5

--- fileutils.sh
+++ fileutils.sh
@@ -6,6 +6,9 @@ COLOR_MODE=`grep ^COLOR $SYS_RC_FILE |head -n 1|cut -c 7-`
 
 [ -r $USER_RC_FILE ] && COLOR_MODE=`grep ^COLOR $USER_RC_FILE |head -n 1|cut -c 7-`
 
+# 'all' argument for 'ls --color=' is no longer valid
+[ "$COLOR_MODE" = all ] && COLOR_MODE=always
+
 [ -z "$COLOR_MODE" ] && COLOR_MODE=$DEF_COLOR_MODE
 
 alias ls="ls --color=$COLOR_MODE"
